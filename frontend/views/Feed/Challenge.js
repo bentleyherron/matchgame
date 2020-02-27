26b6c506743ba3754baa7838e09d4065eebdbfef
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-27 16:04:35 -0500

    Adds challenge list and challenges that workish

--- frontend/views/Feed/Challenge.js
+++ frontend/views/Feed/Challenge.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Content, Text } from 'native-base';
+import { Card, CardItem, Header, Body, H1, Text, car } from 'native-base';
 
-export default function Challenge() {
+export default function Challenge({challenge}) {
+    const {teamFrom, teamTo, date, wager, message} = challenge;
+    const formattedDate = `Date: ${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} at ${date.getHours()}:00`
     return (
-        <Content>
-            <Text>This is a challenge!</Text>
-        </Content>
+        <Card>
+            <CardItem header>
+                <H1>{teamFrom} challenges {teamTo}</H1>
+            </CardItem>
+            <CardItem cardBody>
+                    {wager ? <Text>Wager: {wager}</Text> : null}
+            </CardItem>
+            <CardItem cardBody>
+                    <Text>{formattedDate}</Text>
+            </CardItem>
+            <CardItem cardBody>
+                    <Text>{message}</Text>
+            </CardItem>
+        </Card>
     );
 }
--- frontend/views/Feed/ChallengesContainer.js
+++ frontend/views/Feed/ChallengesContainer.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Content, Text } from 'native-base'; 
+import { Content } from 'native-base'; 
 import Challenge from './Challenge';
 
 export default function ChallengesContainer() {
+    const challenge1 = {
+        teamFrom: 'Shark',
+        teamTo: 'Dolphin',
+        date: new Date("2018-3-20"),
+        message: 'We are going to school you guys!',
+        wager: 100,
+        isAccepted: false
+    };
+
+    const challenge2 = {
+        teamFrom: 'Team Rocket',
+        teamTo: 'Psyduck',
+        date: new Date("2004-2-14"),
+        message: 'Blasting off at the speed of light!',
+        wager: 100,
+        isAccepted: false
+    }
+
+    const challenge3 = {
+        teamFrom: 'Your Mom',
+        teamTo: 'Chad',
+        date: new Date(),
+        message: 'This is getting interesting!',
+        wager: 100,
+        isAccepted: false
+    }
+
+    const challengeList = [challenge1, challenge2, challenge3];
+    const challengeCards = challengeList.map(item => <Challenge challenge={item} />)
     return (
         <Content>
-            <Text>This is the Challenges Container!</Text>
-            <Challenge />
+            {challengeCards}
         </Content>
     );
 }
