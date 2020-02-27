9558869bb41a0e44737412bb2f7bb8c1fc941754
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-27 09:44:20 -0500

    Adds the leaderboard

--- frontend/views/Feed/FeedContainer.js
+++ frontend/views/Feed/FeedContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Leaderboard from './Leaderboard';
 
 export default function FeedContainer({styles}){
     return (
         <View style={styles.container}>
             <Text style={styles.instructions}>This is the feed container.</Text>
+            <Leaderboard styles={styles} />
         </View>
     );
 }
