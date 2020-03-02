a72983c9d66cee79c337c71908ecf7065a687955
David Schneider <david.jay.schneider@gmail.com>
2020-03-02 13:39:11 -0500

    updates event page style

--- frontend/views/Event/EventContainer.js
+++ frontend/views/Event/EventContainer.js
@@ -1,49 +1,41 @@
 import React, { Component } from 'react';
-import { View }  from 'react-native';
-import { Container, Header, Col, Row, Grid, Button, Text, H1, H2, H3, Thumbnail } from 'native-base';
+import { Container, Header, Content, Col, Row, Grid, Button, Text, H1, H2, H3, Thumbnail, Body, Card, CardItem, Item, Left, Right } from 'native-base';
 
 export default class EventContainer extends Component {
   render() {
     return (
       <Container>
         <Header />
-        <Grid style={{alignItems: 'center', padding: 10}}>
-          <Row><H1>Flag Football in the Park</H1></Row>
-          <Row>
-            <Col>
-                {/* <Row><H3>Piedmont Park</H3></Row>
-                <Row><H3>March 15 | 3:30pm</H3></Row> */}
-                <Text>Piedmont Park</Text>
-                <Text>March 15 | 3:30pm </Text>
-            </Col>
-            <Col>
-                <H3>50 WP</H3>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Thumbnail large source={require("../Profile/MightyDucks.png")}/>
-            </Col>
-            <Col style={{alignItems: 'center', justifyContent: 'center'}}><Text>VS</Text></Col>
-            <Col style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Thumbnail large source={require("../Profile/MightyDucks.png")}/>
-            </Col>
-          </Row>
-          <Row>
+        <Content padder>
+          <Card>
+            <CardItem header bordered>
+              <Body>
+                <Text>Piedmont Park Soccer</Text>
+                <Text note>March 15, 3:30pm</Text>
+              </Body>
+              <Right><Thumbnail source={require("../Profile/soccer.png")}></Thumbnail></Right>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text>Points Wagered:</Text>
+              </Body>
+              <Right><H2>100</H2></Right>
+            </CardItem>
+            <CardItem header>
+              <Text>Teams</Text>
+            </CardItem>
+            <CardItem bordered> 
+              <Left><Thumbnail large source={require("../Profile/MightyDucks.png")}></Thumbnail></Left>
+              <Right><Thumbnail large source={require("../Profile/MightyDucks.png")}></Thumbnail></Right>
+            </CardItem>
+            <CardItem>
               <Text>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
               </Text>
-            </Row>
-          <Row>
-              <Col>
-                <Button rounded style={{alignItems: 'center', justifyContent: 'center'}}><Text>Map Event</Text></Button>
-              </Col>
-              <Col>
-                <Button rounded style={{alignItems: 'center', justifyContent: 'center'}}><Text>Cancel</Text></Button> 
-              </Col>
-              </Row>
-        </Grid>
+            </CardItem>
+          </Card>
+          <Button rounded><Text>Cancel</Text></Button>
+        </Content>
       </Container>
     );
   }
