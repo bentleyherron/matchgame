54a82f25e0f7a7348a78d8e399f4c9f9b1a8511a
David Schneider <david.jay.schneider@gmail.com>
2020-03-08 19:03:25 -0400

    add eventTeams get to Events Container

--- frontend/views/Feed/EventsContainer.js
+++ frontend/views/Feed/EventsContainer.js
@@ -1,68 +1,44 @@
-import React from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import Event from './Event';
 import { FlatList } from 'react-native';
 import { Content, Header, Tab, Tabs } from 'native-base';
+import axios from 'axios';
+import {URL} from 'react-native-dotenv';
+import UserContext from '../../UserContext';
 
 export default function EventsContainer() {
-    const event1 = {
-        id: 1,
-        title: "First Event",
-        sport: "kuub",
-        location: "Atlanta",
-        date: new Date('2020-02-15'),
-        description: "Two teams that are ready to rumble",
-        photo: 'https://picsum.photos/200',
-        isPublic: true,
-        eventOccurred: new Date('2020-02-15'),
-        teams: ['sharks', 'dolphins'],
-        wager: 200,
-        winner: 'sharks'
-    };
 
-    const event2 = {
-        id: 2,
-        title: "Second Event",
-        sport: 'wrestling',
-        location: "Atlanta",
-        date: new Date('2020-02-13'),
-        description: "We compete for blood!",
-        photo: 'https://picsum.photos/200',
-        isPublic: true,
-        eventOccurred: new Date('2020-02-13'),
-        teams: ['rocket', 'psyduck'],
-        wager: 350,
-        winner: 'psyduck'
-    };
+    const [eventArray, setEventArray] = useState('');
 
-    const event3 = {
-        id: 3,
-        title: "Rumble in the Jungle",
-        sport: 'boxing',
-        location: "Atlanta",
-        date: new Date('2020-02-02'),
-        description: "Ali vs Frasier",
-        photo: 'https://picsum.photos/200',
-        isPublic: false,
-        eventOccurred: new Date('2020-02-02'),
-        teams: ['ali', 'frasier'],
-        wager: 1000,
-        winner: 'ali'
-    };
+    const { userData, favoriteSports } = useContext(UserContext).state;
 
-    const event4 = {
-        id: 4,
-        title: "Fifa grand final",
-        sport: 'soccer',
-        location: "Atlanta",
-        date: new Date('2020-02-29'),
-        description: "Ali vs Frasier",
-        photo: 'https://picsum.photos/200',
-        isPublic: true,
-        eventOccurred: null,
-        teams: ['spain', 'germany'],
-        wager: 100,
-        winner: null
-    };
+    const getEventTeams = (event_id) => {
+        return axios.get(`${URL}/events/${event_id}`)
+      }
+
+    useEffect(() => {
+        axios.get(`${URL}/events`)
+            .then((response) => {
+                console.log(response.data);
+                // setEventArray(response.data);
+                return (response.data)
+            })
+            .then((response) => {
+                axios.all(response.map((event) => {
+                    return getEventTeams(event.id);
+                }))
+                .then(responseArr => {
+                    responseArr.map(event => {
+                        console.log(event.data)
+                        return (event.data);
+                    })
+                })
+                .then(response => {
+                    setEventArray(response);
+                })
+            })
+
+    },[])
 
     expandEvent = () => {
         console.log('you clicked the event');
@@ -71,7 +47,7 @@ export default function EventsContainer() {
     return (
         <FlatList
         style={{padding: 5}}
-        data={[event1, event2, event3, event4]}
+        data={eventArray}
         renderItem={ ({ item }) => (
             <Event
             keyExtractor={item.id}
