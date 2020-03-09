1c3bac05722403dd2e6e2e44010f0f2a1afd494d
David Schneider <david.jay.schneider@gmail.com>
2020-03-08 23:09:10 -0400

    adds team names to event card

--- frontend/views/Feed/Event.js
+++ frontend/views/Feed/Event.js
@@ -1,13 +1,61 @@
-import React from 'react';
-import { Text, Card, CardItem, H1, Button, Left, Right, Body} from 'native-base';
+import React, {useState, useEffect} from 'react';
+import { Text, Card, CardItem, H1, Button, Left, Right, Body, Spinner} from 'native-base';
+import {URL} from 'react-native-dotenv';
+import axios from 'axios';
 
-export default function Event({event, expandEvent}) {
+export default function Event({eventObject}) {
+
     const { title,
-            location,
             date,
             teams,
-            wager } = event;
-    const formattedDate = date ? `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} | ${date.getHours()}:00` : null;
+            wager,
+            description,
+            sport_id,
+         } = eventObject.event;
+
+
+    const [teamNames, setTeamNames] = useState('');
+
+    const month = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec'];
+    const datetime = new Date(date);
+    const formattedDate = `${month[datetime.getMonth()]} ${datetime.getDate()}`;
+
+    function addZero(i) {
+        if (i < 10) {
+        i = "0" + i;
+        }
+        return i;
+    }
+    
+    function formatTime(d) {
+        let hours = addZero(d.getHours());
+        let minutes = addZero(d.getMinutes());
+        const ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        const formattedTime = hours + ':' + minutes + ' ' + ampm;
+        return formattedTime;
+    }
+
+    const formattedTime = formatTime(datetime);
+
+    useEffect(async () => {
+        try {
+            const teamInfo = await Promise.all(
+                eventObject.eventTeams.map(async team => {
+                    // console.log(`Event: ${team.event_id} Team Id: ${team.team_id}`);
+                    const response = await axios.get(`${URL}/teams/${team.team_id}`);
+                    return response.data;
+                }));
+            setTeamNames(teamInfo);
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }, [])
+
+
     return (
         <Card
             style={{padding: 5}} 
@@ -15,11 +63,12 @@ export default function Event({event, expandEvent}) {
             <CardItem header>
                 <Body>
                     <Text>{title}</Text>
-                    <Text note>{formattedDate ? formattedDate : null}</Text>
-                    <Text note>{teams.join(', ')} </Text>
-                    <Text note>{location}</Text>
+                    {/* {
+                        teamNames ? (<Text>{teamNames[0].name} vs. {teamNames[1].name}</Text>) : null
+                    } */}
+                    <Text note>{formattedDate} · {formattedTime}</Text>
                 </Body>
-                <Right><Button rounded><Text>50 POINTS</Text></Button></Right>
+                <Right><Button rounded><Text>Wager Pts.</Text></Button></Right>
             </CardItem>
         </Card>
     );
